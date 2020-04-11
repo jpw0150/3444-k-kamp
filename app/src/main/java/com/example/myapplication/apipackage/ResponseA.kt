adb7d6eb1c83c8b3f3a7b67582f0e088e3f82dbd
Kevin Gonzalez <kevingonzalez2@my.unt.edu>
2020-04-11 17:58:11 -0500

    Added API

---
+++ app/src/main/java/com/example/myapplication/apipackage/ResponseA.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication.apipackage
+
+import com.google.gson.annotations.SerializedName
+data class Item(val id: Int, val name: String, val cost: Float, val descrip: String)
+data class Customer(val id: Int, val phone: String, val name: String, val password: String, val birthday: String, val visited: Int, val credits: Int)
+data class Employee(val id: Int, val password: String, val name: String, val wage: Float, val role: String, val time: String)
+data class Order(val id: Int, val tableNum: Int, val entree: String, val side: String, val drink: String, val orderTotal: Float)
+data class Ingredient(val id: Int, val food: String, val amount: Int)
+data class ResponseBase(@SerializedName("error") val error: Boolean, val message: String)
+data class ResponseItem(@SerializedName("error") val error: Boolean, val message: String, val item: Item)
+data class ResponseItems(@SerializedName("error") val error: Boolean, val message: String, val items: List<Item>)
+data class ResponseCustomer(@SerializedName("error") val error: Boolean, val message: String, val customer: Customer)
+data class ResponseCustomers(@SerializedName("error") val error: Boolean, val message: String, val customers: List<Customer>)
+data class ResponseEmployee(@SerializedName("error") val error: Boolean, val message: String, val employee: Employee)
+data class ResponseEmployees(@SerializedName("error") val error: Boolean, val message: String, val employees: List<Employee>)
+data class ResponseOrder(@SerializedName("error") val error: Boolean, val message: String, val order: Order)
+data class ResponseOrders(@SerializedName("error") val error: Boolean, val message: String, val orders: List<Order>)
+data class ResponseIngredients(@SerializedName("error") val error: Boolean, val message: String, val ingredient: Ingredient)
+data class ResponseIngredient(@SerializedName("error") val error: Boolean, val message: String, val Ingredients: List<Ingredient>)
