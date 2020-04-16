429d93fd938aae67ade3f39f7263e1ac85b66d81
jpw0150 <61565603+jpw0150@users.noreply.github.com>
2020-04-16 13:01:37 -0500

    Update buildTables.sql

--- buildTables.sql
+++ buildTables.sql
@@ -4,6 +4,7 @@ CREATE TABLE menu (
 	cost float NOT NULL,
 	descrip text,
 	CONSTRAINT menu_pk PRIMARY KEY (id)
+	
 );
 
 CREATE TABLE employee (
@@ -33,20 +34,159 @@ CREATE TABLE orders (
 	entree text,
 	side text,
 	drink text,
+	note text,
 	orderTotal float NOT NULL,
 	CONSTRAINT orders_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE ingredients (
-	id int NOT NULL AUTO_INCREMENT,
+	id int NOT NULL,
 	food text NOT NULL,
 	amount int NOT NULL,
-	CONSTRAINT ing_pk PRIMARY KEY (id)
+	CONSTRAINT ingredients_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE ingredients (
-	id int NOT NULL AUTO_INCREMENT,
-	food text NOT NULL,
-	amount int NOT NULL,
-	CONSTRAINT ing_pk PRIMARY KEY (id)
+CREATE TABLE storeTables (
+	number int NOT NULL,
+	status varchar(30) NOT NULL,
+	needHelp bit,
+	needRefill bit,
+	orderTotal float,
+	CONSTRAINT table_pk PRIMARY KEY (number)
+);
+
+INSERT INTO ingredients
+(id, food, amount)
+VALUES(
+	1,
+	'bone in',
+	100
+);
+
+INSERT INTO ingredients
+(id, food, amount)
+VALUES(
+	2,
+	'boneless',
+	100
+);
+
+INSERT INTO ingredients
+(id, food, amount)
+VALUES(
+	3,
+	'tenders',
+	100
+);
+
+INSERT INTO ingredients
+(id, food, amount)
+VALUES(
+	10,
+	'barbecue sauce',
+	-1
+);
+
+INSERT INTO ingredients
+(id, food, amount)
+VALUES(
+	20,
+	'buffalo sauce',
+	-1
+);
+
+INSERT INTO ingredients
+(id, food, amount)
+VALUES(
+	30,
+	'cajun sauce',
+	-1
+);
+
+INSERT INTO ingredients
+(id, food, amount)
+VALUES(
+	40,
+	'garlic parmesan sauce',
+	-1
+);
+
+INSERT INTO ingredients
+(id, food, amount)
+VALUES(
+	50,
+	'hawaiian sauce',
+	-1
+);
+
+INSERT INTO ingredients
+(id, food, amount)
+VALUES(
+	60,
+	'lemmon pepper sauce ',
+	-1
+);
+
+INSERT INTO ingredients
+(id, food, amount)
+VALUES(
+	100,
+	'ranch',
+	100
+);
+
+INSERT INTO ingredients
+(id, food, amount)
+VALUES(
+	200,
+	'blue cheese',
+	100
+);
+
+INSERT INTO ingredients
+(id, food, amount)
+VALUES(
+	401,
+	'french fries',
+	100
+);
+
+INSERT INTO ingredients
+(id, food, amount)
+VALUES(
+	402,
+	'veggie sticks',
+	100
+);
+
+INSERT INTO ingredients
+(id, food, amount)
+VALUES(
+	403,
+	'corn',
+	100
+);
+
+INSERT INTO ingredients
+(id, food, amount)
+VALUES(
+	501,
+	'soda',
+	100
+);
+
+INSERT INTO ingredients
+(id, food, amount)
+VALUES(
+	502,
+	'tea',
+	100
+);
+
+INSERT INTO ingredients
+(id, food, amount)
+VALUES(
+	503,
+	'lemonade',
+	100
 );
