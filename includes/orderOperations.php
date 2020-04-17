fd22d64897b5198b47821936a1847f4c6c4030e5
jpw0150 <61565603+jpw0150@users.noreply.github.com>
2020-04-17 12:01:56 -0500

    Update orderOperations.php

--- includes/orderOperations.php
+++ includes/orderOperations.php
@@ -9,31 +9,72 @@
 			$this->con = $db->connect();
 		}
 		
-		public function createOrder($tableNum, $entree, $side, $drink, $orderTotal){
-			$state = $this->con->prepare("INSERT INTO orders (tableNum, entree, side, drink, orderTotal) VALUES (?, ?, ?, ?, ?)");
+		public function createOrder($tableNum, $entree, $side, $drink, $note, $orderTotal){
+			$state = $this->con->prepare("INSERT INTO orders (tableNum, entree, side, drink, note, orderTotal) VALUES (?, ?, ?, ?, ?, ?)");
 			
-			$state->bind_param("isssd", $tableNum, $entree, $side, $drink, $orderTotal);
+			$state->bind_param("issssd", $tableNum, $entree, $side, $drink, $note, $orderTotal);
 			
 			if($state->execute()){
 				return ORDER_CREATE;
 			}
 			else{
 				return ORDER_FAIL;
-			}			
+			}
 		}
 		
 		public function allorders(){
+			include_once dirname(__FILE__) . '/ingredientsOperations.php';
 			$state = $this->con->prepare("SELECT * FROM orders");
 			$state->execute();
-			$state->bind_result($id, $tableNum, $entree, $side, $drink, $orderTotal);			
+			$state->bind_result($id, $tableNum, $entree, $side, $drink, $note, $orderTotal);			
 			$orders = array();
+			$ing = new ingredientsOperations;
 			while($state->fetch()){
-				$order = array();
+			$order = array();
+			$entreeData = array();
+			$sideData = array();
+			$drinkData = array();
+			foreach(explode(' ', $entree) as $i){
+				$thisItem = array();
+				$typeId = $i % 10;
+				$flavorId = ($i - $typeId) %100;
+				$sauceId = ($i - $typeId - $flavorId) %1000;
+				$amount = $i - $typeId - $flavorId - $sauceId;
+				$amount = $amount/1000;
+				$flavor = $ing->getInfo($flavorId);
+				$type = $ing->getInfo($typeId);
+				$sauce = $ing->getInfo($sauceId);
+				
+				$thisItem['meatType'] = $type['food'];
+				$thisItem['flavor'] = $flavor['food'];
+				$thisItem['quantity'] = $type['amount'];
+				$thisItem['sauceType'] = $sauce['food'];
+				$thisItem['sauceQuantity'] = $sauce['amount'];
+				$thisItem['price'] = $amount * 1.99;
+				array_push($entreeData, $thisItem);
+			}
+			
+			foreach(explode(' ', $side) as $i){
+				$thisSide = array();
+				$side = $ing->getInfo($i);
+				$thisSide['item'] = $side['food'];
+				$thisSide['quantity'] = $side['amount'];
+				array_push($sideData, $thisSide);
+			}
+			
+			foreach(explode(' ', $drink) as $i){
+				$thisDrink = array();
+				$drink = $ing->getInfo($i);
+				$thisDrink['item'] = $drink['food'];
+				$thisDrink['quantity'] = $drink['amount'];
+				array_push($drinkData, $thisDrink);
+			}
 				$order['id'] = $id;
 				$order['tableNum'] = $tableNum;
-				$order['entree'] = $entree;
-				$order['side'] = $side;
-				$order['drink'] = $drink;
+				$order['entree'] = $entreeData;
+				$order['side'] = $sideData;
+				$order['drink'] = $drinkData;
+				$order['note'] = $note;
 				$order['orderTotal'] = $orderTotal;
 				array_push($orders, $order);
 			}
@@ -41,17 +82,60 @@
 		}
 		
 		public function findOrder($id){
+			include_once dirname(__FILE__) . '/ingredientsOperations.php';
 			$state = $this->con->prepare("SELECT * FROM orders WHERE id = ?");
 			$state->bind_param("i", $id);
 			$state->execute();
-			$state->bind_result($id, $tableNum, $entree, $side, $drink, $orderTotal);
+			$state->bind_result($id, $tableNum, $entree, $side, $drink, $note, $orderTotal);
 			$state->fetch();
 			$order = array();
+			$entreeData = array();
+			$sideData = array();
+			$drinkData = array();
+			$ing = new ingredientsOperations;
+			
+			foreach(explode(' ', $entree) as $i){
+				$thisItem = array();
+				$typeId = $i % 10;
+				$flavorId = ($i - $typeId) %100;
+				$sauceId = ($i - $typeId - $flavorId) %1000;
+				$amount = $i - $typeId - $flavorId - $sauceId;
+				$amount = $amount/1000;
+				$flavor = $ing->getInfo($flavorId);
+				$type = $ing->getInfo($typeId);
+				$sauce = $ing->getInfo($sauceId);
+				
+				$thisItem['meatType'] = $type['food'];
+				$thisItem['flavor'] = $flavor['food'];
+				$thisItem['quantity'] = $type['amount'];
+				$thisItem['sauceType'] = $sauce['food'];
+				$thisItem['sauceQuantity'] = $sauce['amount'];
+				$thisItem['price'] = $amount * 1.99;
+				array_push($entreeData, $thisItem);
+			}
+			
+			foreach(explode(' ', $side) as $i){
+				$thisSide = array();
+				$side = $ing->getInfo($i);
+				$thisSide['item'] = $side['food'];
+				$thisSide['quantity'] = $side['amount'];
+				array_push($sideData, $thisSide);
+			}
+			
+			foreach(explode(' ', $drink) as $i){
+				$thisDrink = array();
+				$drink = $ing->getInfo($i);
+				$thisDrink['item'] = $drink['food'];
+				$thisDrink['quantity'] = $drink['amount'];
+				array_push($drinkData, $thisDrink);
+			}
+			
 			$order['id'] = $id;
 			$order['tableNum'] = $tableNum;
-			$order['entree'] = $entree;
-			$order['side'] = $side;
-			$order['drink'] = $drink;
+			$order['entree'] = $entreeData;
+			$order['side'] = $sideData;
+			$order['drink'] = $drinkData;
+			$order['note'] = $note;
 			$order['orderTotal'] = $orderTotal;
 			return $order;
 		}
@@ -65,9 +149,9 @@
 			return false;
 		}
 		
-		public function updateorder($id, $tableNum, $entree, $side, $drink, $orderTotal){
-            $state = $this->con->prepare("UPDATE orders SET tableNum = ?, entree = ?, side = ?, drink = ?, orderTotal = ? WHERE id = ?");
-            $state->bind_param("isssdi", $tableNum, $entree, $side, $drink, $orderTotal, $id);
+		public function updateorder($id, $tableNum, $entree, $side, $drink, $note, $orderTotal){
+            $state = $this->con->prepare("UPDATE orders SET tableNum = ?, entree = ?, side = ?, drink = ?, note = ?, orderTotal = ? WHERE id = ?");
+            $state->bind_param("issssdi", $tableNum, $entree, $side, $drink, $note, $orderTotal, $id);
             if($state->execute())
                 return true;
             return false; 
