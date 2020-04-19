ee2fdaffbf8e625c0770165e51b808687cf72b68
jpw0150 <61565603+jpw0150@users.noreply.github.com>
2020-04-19 04:17:21 -0500

    Update orderOperations.php

--- includes/orderOperations.php
+++ includes/orderOperations.php
@@ -16,6 +16,69 @@
 			else return false;
 		}
 		
+				public function chefOrders(){
+			include_once dirname(__FILE__) . '/ingredientsOperations.php';
+			$state = $this->con->prepare("SELECT * FROM orders WHERE status = 0");
+			$state->execute();
+			$state->bind_result($id, $tableNum, $entree, $side, $drink, $note, $orderTotal, $status);			
+			$orders = array();
+			$ing = new ingredientsOperations;
+			while($state->fetch()){
+			$order = array();
+			$entreeData = array();
+			$sideData = array();
+			$drinkData = array();
+			foreach(explode(' ', $entree) as $i){
+				
+				$thisItem = array();
+				$typeId = $i % 10;
+				$flavorId = ($i - $typeId) %100;
+				$sauceId = ($i - $typeId - $flavorId) %1000;
+				$amount = $i - $typeId - $flavorId - $sauceId;
+				$amount = $amount/1000;
+				$flavor = $ing->getInfo($flavorId);
+				$type = $ing->getInfo($typeId);
+				$sauce = $ing->getInfo($sauceId);
+				
+				$thisItem['meatType'] = $type['food'];
+				$thisItem['flavor'] = $flavor['food'];
+				$thisItem['quantity'] = $type['amount'];
+				$thisItem['sauceType'] = $sauce['food'];
+				$thisItem['sauceQuantity'] = $sauce['amount'];
+				$thisItem['price'] = $amount * 1.99;
+				array_push($entreeData, $thisItem);
+				
+				
+			}
+			
+			foreach(explode(' ', $side) as $i){
+				$thisSide = array();
+				$side = $ing->getInfo($i);
+				$thisSide['item'] = $side['food'];
+				$thisSide['quantity'] = $side['amount'];
+				array_push($sideData, $thisSide);
+			}
+			
+			foreach(explode(' ', $drink) as $i){
+				$thisDrink = array();
+				$drink = $ing->getInfo($i);
+				$thisDrink['item'] = $drink['food'];
+				$thisDrink['quantity'] = $drink['amount'];
+				array_push($drinkData, $thisDrink);
+			}
+				$order['id'] = $id;
+				$order['tableNum'] = $tableNum;
+				$order['entree'] = $entreeData;
+				$order['side'] = $sideData;
+				$order['drink'] = $drinkData;
+				$order['note'] = $note;
+				$order['orderTotal'] = $orderTotal;
+				$order['status'] = $status;
+				array_push($orders, $order);
+			}
+			return $orders;
+		}
+		
 		public function serve(){
 			$state = $this->con->prepare("SELECT tableNum FROM orders WHERE status = 1");
 			$state->execute();
